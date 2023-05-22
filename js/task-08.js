2163c5237dd8ebe2e3c2dda2d4d5b536debf4e86
YanaBezverkha <126686256+YanaBezverkha@users.noreply.github.com>
2023-05-22 21:21:08 +0100

    destructurisation 8 task

--- js/task-08.js
+++ js/task-08.js
@@ -1,18 +1,17 @@
 const formEl = document.querySelector(".login-form");
-const inputEmailEl = document.querySelector('[name="email"]');
-const inputPasswordEl = document.querySelector('[name="password"]');
+const {email, password} = formEl
 
 formEl.addEventListener("submit", onSubmit);
 
 function onSubmit(event) {
   event.preventDefault();
-  if (inputEmailEl.value === "" || inputPasswordEl.value === "") {
+  if (email.value === "" || password.value === "") {
    return alert("Всі поля повинні бути заповнені.");
   }
 
   const inputValue = {
-    [formEl.elements.email.name]: formEl.elements.email.value,
-    [formEl.elements.password.name]: formEl.elements.password.value,
+    [email.name]: email.value,
+    [password.name]: password.value,
   };
   console.log(inputValue);
   formEl.reset();
